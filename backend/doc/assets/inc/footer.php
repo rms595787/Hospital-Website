cbc44b9a47eb4f396b6a93e9b000fac31bd68634
Rahul Sharma <rms595787+github@gmail.com>
2023-10-03 19:58:43 +0530

    .

---
+++ backend/doc/assets/inc/footer.php
@@ -0,0 +1,11 @@
+
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                2022 - <?php echo date('Y'); ?> &copy; Hospital Management Information System. Developed By <a href="https://developerrony.com">MH RONY</a></a>
+            </div>
+            
+        </div>
+    </div>
+</footer>
